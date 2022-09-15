89b3832c40ed388d2543b2bab666a3343e915c02
Jeavon Leopold <jeavon@crumpled-dog.com>
2022-09-15 10:42:10 +0100

    Improved sample for bundle creation so it doesn't run when Umbraco is upgrading etc

--- Umbraco.Docs.Samples.Web/Stylesheets-Javascript/CreateBundlesNotificationHandler.cs
+++ Umbraco.Docs.Samples.Web/Stylesheets-Javascript/CreateBundlesNotificationHandler.cs
@@ -1,5 +1,7 @@
-﻿using Umbraco.Cms.Core.Events;
+﻿using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.WebAssets;
 
 // https://our.umbraco.com/documentation/Fundamentals/Design/Stylesheets-Javascript/#bundling--minification-for-javascript-and-css
@@ -8,17 +10,24 @@ namespace Umbraco.Docs.Samples.Web.Stylesheets_Javascript
     public class CreateBundlesNotificationHandler : INotificationHandler<UmbracoApplicationStartingNotification>
     {
         private readonly IRuntimeMinifier _runtimeMinifier;
+        private readonly IRuntimeState _runtimeState;
 
-        public CreateBundlesNotificationHandler(IRuntimeMinifier runtimeMinifier) => _runtimeMinifier = runtimeMinifier;
+        public CreateBundlesNotificationHandler(IRuntimeMinifier runtimeMinifier, IRuntimeState runtimeState) {
+            _runtimeMinifier = runtimeMinifier;
+            _runtimeState = runtimeState;
+        }
         public void Handle(UmbracoApplicationStartingNotification notification)
         {
-            _runtimeMinifier.CreateJsBundle("inline-js-bundle",
+            if (_runtimeState.Level == RuntimeLevel.Run)
+            {
+                _runtimeMinifier.CreateJsBundle("inline-js-bundle",
                 BundlingOptions.NotOptimizedAndComposite,
                 new[] { "~/scripts/test-script1.js", "~/scripts/test-script2.js" });
 
-            _runtimeMinifier.CreateCssBundle("inline-css-bundle",
-                BundlingOptions.NotOptimizedAndComposite,
-                new[] { "~/css/test-style.css" });
+                _runtimeMinifier.CreateCssBundle("inline-css-bundle",
+                    BundlingOptions.NotOptimizedAndComposite,
+                    new[] { "~/css/test-style.css" });
+            }
         }
     }
 }
