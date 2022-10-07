9df274430b6080f02e3da09cecb16ce58ae27eba
Busra <busra@bumpdigital.co.uk>
2022-10-07 16:16:07 +0300

    Added custom examine index samples

---
+++ Umbraco.Docs.Samples.Web/CustomIndexing/ConfigureProductIndexOptions.cs
@@ -0,0 +1,58 @@
+﻿using Examine;
+using Examine.Lucene;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Util;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Scoping;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.Examine;
+
+namespace Umbraco.Docs.Samples.Web.CustomIndexing
+{
+    public class ConfigureProductIndexOptions : IConfigureNamedOptions<LuceneDirectoryIndexOptions>
+    {
+        private readonly IOptions<IndexCreatorSettings> _settings;
+        private readonly IPublicAccessService _publicAccessService;
+        private readonly IScopeProvider _scopeProvider;
+
+        public ConfigureProductIndexOptions(
+            IOptions<IndexCreatorSettings> settings,
+            IPublicAccessService publicAccessService,
+            IScopeProvider scopeProvider)
+        {
+            _settings = settings;
+            _publicAccessService = publicAccessService;
+            _scopeProvider = scopeProvider;
+        }
+
+        public void Configure(string name, LuceneDirectoryIndexOptions options)
+        {
+            if (name.Equals("ProductIndex"))
+            {
+                options.Analyzer = new StandardAnalyzer(LuceneVersion.LUCENE_48);
+
+                options.FieldDefinitions = new(
+                    new("id", FieldDefinitionTypes.Integer),
+                    new("name", FieldDefinitionTypes.FullText)
+                    );
+
+                options.UnlockIndex = true;
+
+                options.Validator = new ContentValueSetValidator(true, false, _publicAccessService, _scopeProvider, includeItemTypes: new[] { "product" });
+                
+                if (_settings.Value.LuceneDirectoryFactory == LuceneDirectoryFactory.SyncedTempFileSystemDirectoryFactory)
+                {
+                    // if this directory factory is enabled then a snapshot deletion policy is required
+                    options.IndexDeletionPolicy = new SnapshotDeletionPolicy(new KeepOnlyLastCommitDeletionPolicy());
+                }
+            }
+        }
+        
+        public void Configure(LuceneDirectoryIndexOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
