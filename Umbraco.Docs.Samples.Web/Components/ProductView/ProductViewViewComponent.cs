31ce4b5dd2b4cae5ecbe0abd8442470b09ec4155
Busra <busra@bumpdigital.co.uk>
2022-10-18 10:13:21 +0300

    Added ViewComponent Sample

---
+++ Umbraco.Docs.Samples.Web/Components/ProductView/ProductViewViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Umbraco.Docs.Samples.Web.Components.ProoductView
+{
+    public class ProductViewViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<string> products = new List<string>() {
+                "Product 1", "Product 2", "Product 3", "Product 4", "Product 5"
+            };
+
+            return View(products);
+        }
+    }
+}
