5a1fc848e48cb80d11ce94c99ffc07c58c12c691
Marc Goodson <marc@snowmoon.co.uk>
2023-04-27 21:31:53 +0100

    path and __Published are required to be in the index if you want to use ContentSetValueValidator
    This code checks path and published when deciding whether to filter the document without them nothing is indexed!

--- Umbraco.Docs.Samples.Web/CustomIndexing/ProductIndexValueSetBuilder.cs
+++ Umbraco.Docs.Samples.Web/CustomIndexing/ProductIndexValueSetBuilder.cs
@@ -1,4 +1,4 @@
-using Examine;
+﻿using Examine;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Infrastructure.Examine;
 
@@ -14,6 +14,12 @@ namespace Umbraco.Docs.Samples.Web.CustomIndexing
                 {
                     ["name"] = content.Name,
                     ["id"] = content.Id,
+                    // nodeName used in the Examine Dashboard backoffice search results
+                    ["nodeName"] = content.Name,
+                    //__Published and path are required if using core ContentValueSetValidator to apply a Validation option to filter results
+                    ["__Published"] = content.Published ? "y" : "n",
+                    ["path"] = content.Path
+
                 };
 
                 yield return new ValueSet(content.Id.ToString(), "content",content.ContentType.Alias,indexValues);
